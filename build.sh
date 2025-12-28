b0f4db6152f869ddf8fc92628b75d3aa3d55a01f
maxfun0 <31366029+maxfun0@users.noreply.github.com>
2025-12-28 21:28:32 +0800

    Update build script to change zip file name

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ ndk-build -C module || exit 1
 
 mkdir -p magisk/zygisk
 
-for arch in arm64-v8a armeabi-v7a x86 x86_64
+for arch in arm64-v8a
 do
     cp module/libs/$arch/*.so magisk/zygisk/$arch.so
 done
@@ -19,5 +19,5 @@ name=`grep '^id=' module.prop | cut -d= -f2`
 rm -rf ../build
 mkdir -p ../build
 
-zip -r9 ../build/hmspush-zygisk-$version.zip .
+zip -r9 ../build/mipush-zygisk-$version.zip .
 popd
